a66cedf8d7e887a3b8610e23c3441c8588cbe8fa
pompomtcy <pompomtcy@gmail.com>
2026-03-30 10:22:05 +0700

    Fix formatting issue in app_sum.py

--- app_sum.py
+++ app_sum.py
@@ -3,7 +3,7 @@ st.title("My Discharge Summary 🚀")
 name = st.text_input("Patient's name")
 age = st.text_input("Age")
 
-gender = st.radio("Gender", ["Male", "Female"], horizontal=True)ฃ
+gender = st.radio("Gender", ["Male", "Female"], horizontal=True)
 underlying = st.text_input("U/D")
 history = st.text_input("Present illness")
 admit_date = st.date_input("Admission date")
