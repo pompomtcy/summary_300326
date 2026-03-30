bd07758eb756bd73f9199e0cdaf96642f0521915
pompomtcy <pompomtcy@gmail.com>
2026-03-30 10:21:43 +0700

    Update app_sum.py

--- app_sum.py
+++ app_sum.py
@@ -3,4 +3,8 @@ st.title("My Discharge Summary 🚀")
 name = st.text_input("Patient's name")
 age = st.text_input("Age")
 
-gender = st.radio("Gender", ["Male", "Female"], horizontal=True)
+gender = st.radio("Gender", ["Male", "Female"], horizontal=True)ฃ
+underlying = st.text_input("U/D")
+history = st.text_input("Present illness")
+admit_date = st.date_input("Admission date")
+discharge_date = st.date_input("Discharge date")
