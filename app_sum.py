46b75df9a639b46cf1eb7babcc7374185eecf475
pompomtcy <pompomtcy@gmail.com>
2026-03-30 10:12:17 +0700

    Update app_sum.py

--- app_sum.py
+++ app_sum.py
@@ -1,3 +1,6 @@
 import streamlit as st
 st.title("My Discharge Summary 🚀")
 title = st.text_input("Pateint's name")
+title = st.text_input("Age")
+title = st.text_input("Pateint's name")
+genre = st.radio("Gender"["***Male***","***Female***"])
